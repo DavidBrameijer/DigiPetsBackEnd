c9ed75b2ca6bfefc7c01987d6e0845c28315a20f
Wren <grahamgrasley@gmail.com>
2024-10-02 15:58:48 -0400

    started controller

---
+++ DigiPetsBackEnd/Controllers/PetController.cs
@@ -0,0 +1,19 @@
+﻿using DigiPetsBackEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiPetsBackEnd.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class PetController(DigiPetsDbContext context) : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetAll(int account = 0)
+		{
+			if (account == 0)
+				return Ok(context.Pets);
+			return Ok(context.Pets.Where(p => p.AccountId == account));
+		}
+	}
+}
---
+++ DigiPetsBackEnd/Models/DigiPetsDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigiPetsBackEnd.Models;
+
+public partial class DigiPetsDbContext : DbContext
+{
+    public DigiPetsDbContext()
+    {
+    }
+
+    public DigiPetsDbContext(DbContextOptions<DigiPetsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Pet> Pets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=DigiPetsDB; Integrated Security=SSPI;Encrypt=false;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pet>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Pets__3214EC27B4D57754");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Health).HasColumnType("decimal(18, 0)");
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
